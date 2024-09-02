ac4fcffe2b458ca5a86895dc962059bac4c37079
atulk2022 <113936949+atulk2022@users.noreply.github.com>
2024-09-02 19:58:10 +0530

    Update candidateRoutes.js

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const User = require('../models/user');
 const {jwtAuthMiddleware, generateToken} = require('../jwt');
-const Candidate = require('../models/candidate');
+const Candidate = require('../models/candidate'); 
 
 
 const checkAdminRole = async (userID) => {
