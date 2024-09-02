c15b42498cdc2e6069d85ab9f23989041412f1e4
atulk2022 <113936949+atulk2022@users.noreply.github.com>
2024-09-02 19:58:01 +0530

    Update candidateRoutes.js

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); 
 const User = require('../models/user');
 const {jwtAuthMiddleware, generateToken} = require('../jwt');
 const Candidate = require('../models/candidate');
